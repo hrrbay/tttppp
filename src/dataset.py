ff5d008ef407198d70f476866e47ae812730a23e
Jakob Sattler <jakob.sattler@outlook.com>
2023-12-30 16:12:37 +0100

    load segmentation masks

--- src/dataset.py
+++ src/dataset.py
@@ -5,7 +5,7 @@ import pdb
 from torch.utils.data import Dataset
 from PIL import Image
 import cv2
-
+import scipy.sparse as sp
 
 class TTVid():
 
@@ -18,8 +18,12 @@ class TTVid():
         self.src_fps = src_fps
         self.target_fps = target_fps
 
+        self.in_size = (320, 128) 
+        self.resize = (320, 128) # in case we want to downsample masks -- NOTE that data has to be properly extracted again (and will be of higher size)
+    
+
         # load all frames, create annotations
-        total_frames = [os.path.join(path, frame) for frame in sorted(os.listdir(path))]
+        self.sparse_data = sp.load_npz(os.path.join(path,'t3p3_mask.npz')).tolil()
 
         self.sequences = []
         if labeled_start: # start of sequences are labeled
@@ -31,7 +35,8 @@ class TTVid():
                 next_point_player = annotations['points'][i + 1][0]
                 self.next_points[i // 2] = next_point_player
                 #self.start_end[i // 2] = torch.tensor([start, end])
-                self.sequences.append(total_frames[start:end])
+                self.sequences.append(self.sparse_data[start:end])
+                # self.sequences.append(total_frames[start:end])
         else: # start of sequences are not labeled -> use point of previous sequence as start
             self.next_points = torch.empty(len(annotations['points']), 1, dtype=int)
             #self.start_end = torch.empty(len(annotations['points']), 2, dtype=int)
@@ -44,7 +49,8 @@ class TTVid():
                 next_point_player = annotations['points'][i][0]
                 self.next_points[i] = next_point_player
                 #self.start_end[i] = torch.tensor([start, end])
-                self.sequences.append(total_frames[start:end])
+                self.sequences.append(self.sparse_data[start:end])
+                # self.sequences.append(total_frames[start:end])
 
         # post-process data
         downscale = src_fps // target_fps
@@ -52,9 +58,9 @@ class TTVid():
         self.num_frames = 0
         for i in range(self.num_sequences):
             self.sequences[i] = self.post_process(self.sequences[i], downscale)
-            self.num_frames += len(self.sequences[i])
+            self.num_frames += self.sequences[i].shape[0]
 
-        self.wins_per_seq = [len(seq) + 1 - window_size for seq in self.sequences]
+        self.wins_per_seq = [seq.shape[0] + 1 - window_size for seq in self.sequences]
         self.num_wins = sum(wins for wins in self.wins_per_seq)
         # annoations
         '''self.annotations = { # redundant right now?
@@ -71,6 +77,7 @@ def extract_player_mask(img):
     # get green blobs and sort by area to get players
     green = np.array([60, 255, 255])
     green_mask = cv2.inRange(hsv_image, green, green)
+
     contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
     
@@ -125,42 +132,63 @@ class TTData(Dataset):
             seq_idx += 1
 
         seq = vid.sequences[seq_idx]
-        window_frames = seq[idx:idx+self.win_size]
+        seg_masks = seq[idx:idx+self.win_size]
 
+        # reshape to CDHW for 3d conv -- don't know if explicit channel-dimension of 1 is necessary but better be safe
+        seg_masks = torch.stack([torch.tensor(f.toarray()).reshape(1, vid.resize[1], vid.resize[0]) for f in seg_masks], dim=0)
 
-        window_frames = [Image.open(frame).convert('RGB') for frame in window_frames]                  # This is slow
+        # uncomment this if you want to check how it looks
+        # for f in window_frames.squeeze():
+        #     if idx < 100:
+        #         break
+        #     import matplotlib.pyplot as plt
+        #     plt.imshow(f.cpu().numpy(), cmap='gray')
+        #     plt.show()
+
         label = vid.next_points[seq_idx]
-        assert len(window_frames) == self.win_size
+        assert seg_masks.shape[0] == self.win_size
 
         # TODO: replace window_frames with seg-masks, ball positions
-        return window_frames, label
+        return seg_masks, label
 
 
+def get_grayscale_mask(img, ball_pos=None, ball_radius=10):
+    # set middle thirds to black to get rid of referee
+    img[:,img.shape[1]//3:2*img.shape[1]//3, :] = 0
+    # remove non-green
+    img[img != [0,255,0]] = 0
+    img /= float(255)
+    left_im, right_im = img[:,:img.shape[1]//2][:,:,1] * 0.2, img[:,img.shape[1]//2:][:,:,1] * 0.4
+    grayscale_mask = np.hstack([left_im, right_im])
+    if ball_pos:
+        assert ball_radius > 0
+        x = np.arange(0, img.shape[1])
+        y = np.arange(0, img.shape[0])
+        mask = (x[None,:]-ball_pos[0])**2 + (y[:,None]-ball_pos[1])**2 <= ball_radius
+        grayscale_mask[mask] += 0.6
+    return grayscale_mask
 
 
-'''train_path = '/home/christian/Desktop/TTNet-Real-time-Analysis-System-for-Table-Tennis-Pytorch/dataset/training/images'
-vids = []
-for game in os.listdir(train_path):
-    points = np.loadtxt(f'../point_labels/train_{game[-1]}.txt')
-    annotations = {
-        'points': points.astype(int)# TODO
-    }
-    vids.append(TTVid(os.path.join(train_path, game), annotations, 120, 60, sequences=True))
+'''
+    Some tests
+'''
+path = './data/annotations/test_1'
+points = np.loadtxt('./point_labels/test_1.txt')
+annotations = {
+    'points': points.astype(int)
+}
+vid = TTVid(path, annotations, 120, 60)
+dataset = TTData([vid])
 
-train_dataset = TTData(vids, win_size=30)'''
+import time
+t = time.time()
+total_time = 0
+n = 0
+for a in dataset:
+    cur_t = time.time() - t
+    print(f'{cur_t=}', end='\r')
+    total_time += cur_t
+    t = time.time()
+    n += 1
 
-test_path = '/home/christian/Desktop/TTNet-Real-time-Analysis-System-for-Table-Tennis-Pytorch/dataset/test/images'
-vids = []
-for game in os.listdir(test_path):
-    points = np.loadtxt(f'../point_labels/test_{game[-1]}.txt')
-    annotations = {
-        'points': points.astype(int) # TODO
-    }
-    vids.append(TTVid(os.path.join(test_path, game), annotations, 120, 60, labeled_start=False))
-
-test_dataset = TTData(vids, win_size=30)
-frames, label = test_dataset[0]
-
-for frames, annotations in test_dataset:
-    print(len(frames))
-    # print(annotations)
+print(f'avg load: {total_time / n}')
