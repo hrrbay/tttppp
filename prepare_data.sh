b9790210c09a32bd2d5cbfdefc344dac1bdba759
Jakob Sattler <jakob.sattler@outlook.com>
2024-01-16 20:02:41 +0100

    new data download script

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 data_path=${1:-"./data"}/t3p3
-echo "Downloading and extracting to ${data_path}. You can pass another path as the first argument."
+echo "Downloading and extracting to ${data_path}. You can pass a different download path as the first argument if you want."
 echo "data-path: ${data_path}"
 
 
 # wget form gdrive as described in https://medium.com/@acpanjan/download-google-drive-files-using-wget-3c2c025a8b99
-file_id=1yo9yWHCH3NACFbBa6I_qS1GMwDd-PCg5
+# switch to gdown. Seems to be more reliable than wget
+file_id=1dPysMHuSuKPVnWYtIVMqSVnpnoHbwBMH
 mkdir -p ${data_path}
 out_name=${data_path}/t3p3_data.tar.gz
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=${file_id}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${file_id}" -O ${out_name} && rm -rf /tmp/cookies.txt
+gdown https://drive.google.com/uc?id=$file_id --output $out_name
+# wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=${file_id}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${file_id}" -O ${out_name} && rm -rf /tmp/cookies.txt
 
 # decompress
 cd ${data_path}
