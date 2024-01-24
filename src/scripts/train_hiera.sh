885f4cf16fe7ae75f636d5a973248876ee01e027
Christian Thurner <c.thurner@student.tugraz.at>
2024-01-24 09:19:35 +0100

    readme foobar und training scripts für hiera+tttransformer

--- src/scripts/train_hiera.sh
+++ src/scripts/train_hiera.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-for seed in 1 2 3 4 5 6 7 8 9 10; do
-  python ../main.py \
-  --network HieraB \
-  --data-config hiera \
-  --window-size 16\
-  --model-config ../config/hiera.yaml \
-  --batch-size 32 \
-  --checkpoint-freq 5 \
-  --exp-name hiera_last \
-  --fixed-seq-len 200 \
-  --flip-prob 0.5 \
-  --gpu 1 \
-  --labeled-start \
-  --lr 0.01 \
-  --lr-factor 3 \
-  --lr-min 0.00001 \
-  --lr-patience 6 \
-  --momentum 0.9 \
-  --nepochs 50 \
-  --seed 1 \
-  --src-fps 120 \
-  --target-fps 15 \
-  --use-poses false \
-  --validation 0.1 \
-  --weight-decay 0.0002 \
-  --window-size 16
-done
+
+python ./src/main.py \
+--network HieraB \
+--data-config hiera \
+--window-size 16\
+--model-config ./src/config/hiera.yaml \
+--batch-size 32 \
+--checkpoint-freq 5 \
+--exp-name hiera_last \
+--fixed-seq-len 200 \
+--flip-prob 0.5 \
+--gpu 1 \
+--labeled-start \
+--lr 0.01 \
+--lr-factor 3 \
+--lr-min 0.00001 \
+--lr-patience 6 \
+--momentum 0.9 \
+--nepochs 50 \
+--seed 1 \
+--src-fps 120 \
+--target-fps 15 \
+--use-poses false \
+--validation 0.1 \
+--weight-decay 0.0002 \
+--window-size 16
