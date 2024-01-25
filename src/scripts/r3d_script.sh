e426aec56a932b5bb0c2e1f6b617d3cf69454469
Jakob Sattler <jakob.sattler@outlook.com>
2024-01-25 22:13:01 +0100

    remove useless files

--- src/scripts/r3d_script.sh
+++
@@ -1,3 +0,0 @@
-for seed in 1 2 3 5 6 7 8; do
-    python3 src/main.py --batch-size 32 --lr-patience 6 --lr 0.01 --network r3d_18 --data-config r3d_18 --nepochs 50 --fixed-seq-len 200 --validation 0.1 --target-fps 15 --window-size 16 --seed $seed --flip-prob 0.5 --lr-patience 6 --exp-name r3d_flip_val01_sgd --gpu 0 --lr-min 1e-5
-done
