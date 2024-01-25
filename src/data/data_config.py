0e69600826553adcc0c57356f31023d501239b5c
Jakob Sattler <jakob.sattler@outlook.com>
2024-01-25 21:07:19 +0100

    fix data-path

--- src/data/data_config.py
+++ src/data/data_config.py
@@ -3,13 +3,12 @@ import pdb
 
 from torchvision import transforms
 
-BASE_PATH = os.path.join(os.path.dirname(__file__), '..')
+BASE_PATH = os.path.join(os.path.dirname(__file__), '..', '..', 't3p3_data')
 BASE_PATH = os.path.abspath(BASE_PATH)
 if 'T3P3_PATH' in os.environ:
     BASE_PATH = os.environ['T3P3_PATH']
   
 data_config = {
-    # TODO: add normalization for all
     'base': {
         'path': os.path.join(BASE_PATH, 't3p3'),
         'transforms': [
@@ -21,7 +20,7 @@ data_config = {
         'transforms': [
             transforms.Normalize(mean=0.012362, std=0.076184),
             transforms.Resize((112,112)),
-            transforms.Lambda(lambda x: x.repeat(3, 1, 1)) # TODO: Check this does the correct thing (repeating channel)
+            transforms.Lambda(lambda x: x.repeat(3, 1, 1)) 
         ]
     },
     'hiera': {
@@ -29,7 +28,7 @@ data_config = {
             'transforms': [
                 transforms.Normalize(mean=0.012362, std=0.076184),
                 transforms.Resize((224, 224)),
-                transforms.Lambda(lambda x: x.repeat(3, 1, 1)) # TODO: Check this does the correct thing (repeating channel)
+                transforms.Lambda(lambda x: x.repeat(3, 1, 1)) 
             ]
         },
     'tttransformer': {
